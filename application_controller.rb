07a69b9f75ae6618965c55d4b08038f7a6981be5
allierae <allison.goss@students.loy.org>
2015-07-02 17:59:55 +0000

    Finished home and results page plus some other stuff i guess

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,19 @@
 require 'bundler'
 Bundler.require
+require_relative 'models/model.rb'
 
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
   end
+  
+  get '/quiz' do
+    erb :quiz
+  end 
+  
+  get '/results' do
+    erb :results
+  end 
 
 end
